d1b4f85dbb114d5c883b47ac12f29bdef1ee479c
AnGeD13 <annagetun13@gmail.com>
2024-11-22 21:54:00 +1000

    first_lab

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/7семестр/операционки/labs/lab1
+# Install script for directory: D:/7semestr/os/labs/lab1
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/7семестр/операционки/labs/lab1/build/install_local_manifest.txt"
+  file(WRITE "D:/7semestr/os/labs/lab1/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/7семестр/операционки/labs/lab1/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/7semestr/os/labs/lab1/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
